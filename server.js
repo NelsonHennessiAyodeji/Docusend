6392ccdf10163f92161c2e1fbdc8f939fbd1ba4b
Khomeini N.S.E <81471974+NelsonHennessiAyodeji@users.noreply.github.com>
2023-06-26 21:32:09 +0100

    Database now connected
    Co-Authored-By: Akeem22 <90483300+akeem22@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //Global Imports
-require('dotenv');
+require('dotenv').config();
 
 //Server Runner inports
 const express = require('express');
 const server = express();
+const db = require('./database/connectDB');
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +13,15 @@ server.get('/', (req, res) => {
     res.status(200).json("Docusend Homepage");
 });
 
-server.listen(port, () => {
-    console.log(`Server listening on ${port}`);
-});
+const start = async() => {
+    try {
+        await db(process.env.MONGO_URI);
+        server.listen(port, () => {
+            console.log(`Server listening on ${port}`);
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+start();
